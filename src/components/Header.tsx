67db1f2613439b0a3ed52580f5a84bd3c0d99ab2
rafaelst2000 <rafaelst2000@gmail.com>
2024-02-07 09:02:44 -0300

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <HeaderContainer>
       <div className='logo-container'>
-        <Image src="/images/LogoHeader.svg" width={120} height={42} style={{ maxWidth: '100%', height: 'auto' }}  alt="logo header" />
+        <Image src="/images/LogoHeader.svg" width={120} height={42} style={{ maxWidth: '100%' }}  alt="logo header" />
       </div>
       <Avatar />
     </HeaderContainer>
